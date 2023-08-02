82f8e504134dd085b633ba29844ad28a0611cf17
Jahid Hasan <jahidhasan2856@gmail.com>
2023-08-02 16:01:57 +0600

    adding projects page

--- app/(dashboard)/(routes)/projects/page.tsx
+++ app/(dashboard)/(routes)/projects/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+"use client";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import React from "react";
 
 const page = () => {
-  return <div>Projects </div>
-}
+  return (
+    <div className="py-5">
+      <div className="flex items-center justify-between border-b border-gray-200 pb-3">
+        <h3 className="text-gray-800 text-3xl font-bold">Projects</h3>
+        <div>
+          <button className="flex items-center gap-1 bg-primary px-2.5 rounded text-sm py-2 text-white">
+            <Icon icon="ic:baseline-plus" width={22} />
+            <span>Create Project</span>
+          </button>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ components/ui/MyTab/MyTab.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Tab } from "@headlessui/react";
+
+const MyTab = () => {
+  return (
+    <>
+      <Tab.Group>
+        <Tab.List>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+          <Tab>Tab 3</Tab>
+        </Tab.List>
+        {/* <Tab.Panels>
+          <Tab.Panel>Content 1</Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+          <Tab.Panel>Content 3</Tab.Panel>
+        </Tab.Panels> */}
+      </Tab.Group>
+    </>
+  );
+};
+
+export default MyTab;
